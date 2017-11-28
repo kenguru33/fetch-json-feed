c4b475e67dd7c21946947660193135bc5ec7fa26
Bernt Anker <bernt.anker@me.com>
2017-11-28 17:46:04 +0100

    rearrange code

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const fetchJsonFeed = require('./lib/json-feed-fetcher')
+const axios = require('axios')
+const recursivelyLowercaseJSONKeys = require('recursive-lowercase-json')
+const validator = require('is-my-json-valid')
 
-module.exports = {
-  fetchJsonFeed
+/**
+ * Fetch json filter response against schema.
+ * @param {string} url
+ * @param {*} schema
+ */
+const fetchJsonData = (url, schema) => {
+  const filter = validator.filter({
+    required: true,
+    type: 'object',
+    properties: schema,
+    additionalProperties: false
+  })
+
+  return axios
+    .get(url)
+    .then(response => {
+      return recursivelyLowercaseJSONKeys(response.data)
+    })
+    .then(data => {
+      return data.map(obj => {
+        return filter(obj)
+      })
+    })
 }
+
+module.exports = fetchJsonData
